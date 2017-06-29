44520c54888de90c289530c2517400de1fe91d9d
BigETI <bigeti.ethem@gmail.com>
2017-06-29 13:21:06 +0200

    Projektdateien hinzufügen.

---
+++ WinFormsTranslator/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsTranslator
+{
+    public class Class1
+    {
+    }
+}
